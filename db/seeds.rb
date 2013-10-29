53fb49aa7a4c69fa0336ff7a9ce3d506245903f7
Alistair Duggin <alistair.duggin@btinternet.com>
2013-10-29 00:35:03 +0000

    updating seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Talk.create(title: "Get your Sass in Line", video_url: "http://www.youtube.com/watch?v=ljPpTepThRI#t=775", description: "INSERT", minutes: 71, speaker: "Micah Godbolt")
+Talk.create(title: "The mind blowing Power of Sass 3.3", video_url: "http://www.youtube.com/watch?v=-ZJeOJGazgE", description: "INSERT", minutes: 39, speaker: "Chris Eppstien")
+Talk.create(title: "Adaptive Images in Responsive Web", video_url: "http://www.youtube.com/watch?v=NsCMEK5F8sg", description: "INSERT", minutes: 37, speaker: "Christopher Schmitt")
+Talk.create(title: "Unfolding The Box model", video_url: "http://confreaks.com/videos/2234-html5tx2013-unfold...", description: "INSERT", minutes: 37, speaker: "Chris Ruppel")
+Talk.create(title: "Select This!", video_url: "http://www.youtube.com/watch?v=9s0H0SFkd_g", description: "INSERT", minutes: 56, speaker: "Estrella Way")
+Talk.create(title: "Your css is a mess", video_url: "https://vimeo.com/61755493", description: "CSS is easy. You take some selectors, add some prop...", minutes: 34, speaker: "Jonathan Snook")
